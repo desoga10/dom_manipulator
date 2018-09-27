5cb7762052e45c66266d2936256687ef798941cb
desoga <done2stay@gmail.com>
2018-09-27 22:44:30 +0100

    Created submit event

---
+++ app.js
@@ -0,0 +1,11 @@
+var form = document.getElementById('addForm');
+var item = document.getElementById('items');
+
+//create the submit event
+form.addEventListener('submit', addItems);
+//Add Items
+function addItems(e) {
+  e.preventDefault();
+
+  //Get Input Value
+}
